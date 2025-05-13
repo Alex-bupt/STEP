56f53b810e4c8a8325336904de30e15e7a07374a
Zhenye Yang <77962334+Alex-bupt@users.noreply.github.com>
2025-05-13 18:15:00 +0800

    Add files via upload

---
+++ rec/scripts/pretrain_inspired.sh
@@ -0,0 +1,27 @@
+# cp -r data/inspired src/data/
+# cd src/data/inspired
+cd src
+python data/inspired/process.py
+# cd ../../
+
+# cp -r data/redial src/data/
+# cd src
+# python data/redial /process.py
+
+CUDA_VISIBLE_DEVICES=0 accelerate launch train_pre_inspired.py \
+    --dataset inspired \
+    --tokenizer /root/autodl-tmp/DCRS-main/rec/src/models/DialoGPT-small \
+    --model /root/autodl-tmp/DCRS-main/rec/src/models/DialoGPT-small \
+    --text_tokenizer /root/autodl-tmp/DCRS-main/rec/src/models/roberta-base \
+    --text_encoder /root/autodl-tmp/DCRS-main/rec/src/models/roberta-base \
+    --num_train_epochs 5 \
+    --gradient_accumulation_steps 1 \
+    --per_device_train_batch_size 64 \
+    --per_device_eval_batch_size 64 \
+    --num_warmup_steps 168  \
+    --max_length 200 \
+    --prompt_max_length 200 \
+    --entity_max_length 32 \
+    --learning_rate 6e-4 \
+    --output_dir ./pre-trained-prompt-inspired \
+    --seed 1
