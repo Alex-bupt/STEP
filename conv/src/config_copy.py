971dbbd79115d9c279fdc30bcbe1f79896aec405
Zhenye Yang <77962334+Alex-bupt@users.noreply.github.com>
2025-08-09 21:41:49 +0800

    Delete conv directory

--- conv/src/config_copy.py
+++
@@ -1,8 +0,0 @@
-gpt2_special_tokens_dict = {
-    'pad_token': '<pad>',
-    'additional_special_tokens': ['<movie>','<PAD>','<D>','<R>']
-}
-
-prompt_special_tokens_dict = {
-    'additional_special_tokens': ['<movie>','<mask>','<PAD>'],
-}
